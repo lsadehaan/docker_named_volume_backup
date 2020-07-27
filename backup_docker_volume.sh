3672b3e3343b4e821dd91926b180d32351f69163
adriaan <adriaan.dehaan@gmail.com>
2020-07-26 21:50:32 -0300

    single argument only

--- backup_docker_volume.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm   --volume $1:/dbdata   --volume $(pwd):/backup   ubuntu   tar cvf /backup/$2 /dbdata
--- restore_docker_volume.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm  --volume $1:/dbdata --volume $(pwd):/backup ubuntu  tar xvf /backup/$2 -C /dbdata --strip 1
